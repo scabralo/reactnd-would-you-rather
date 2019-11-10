2ad315ff68205ca124a1223d14bdc21c4bd85670
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-10 17:21:48 -0500

    Adds Initial Actions to project

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -119,6 +119,8 @@ function generateUID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
+export function
+
 export function _getUsers () {
   return new Promise((res, rej) => {
     setTimeout(() => res({...users}), 1000)
