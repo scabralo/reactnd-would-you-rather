0b35bdf25daf27626962a6588c9bd090db7f81d3
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-10 17:34:34 -0500

    Adds Initial Action Creators to project

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,1 +1,13 @@
-import 
+import { getInitialData } from '../utils/_DATA'
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+      })
+  }
+}
