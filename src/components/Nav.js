476b81a6dca02728422ea5246fa4f5eb43da7cd8
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 17:36:40 -0500

    Implements Links in navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Nav extends Component {
   render() {
+    const { authedUser } = this.props
+
     return (
       <div>
-        Navigation
+        <ul>
+          <li><Link to={authedUser ? '/' : '#' }>Home</Link></li>
+          <li><Link to={authedUser ? '/add' : '#'}>Add Question</Link></li>
+        </ul>
       </div>
     )
   }
 }
 
-export default Nav
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
