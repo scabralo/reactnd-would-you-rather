e8e939b2072731a735d756e6db4de590d430a32a
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 13:52:03 -0500

    Adds Nav Comp to project

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        Navigation
+      </div>
+    )
+  }
+}
+
+export default Nav
