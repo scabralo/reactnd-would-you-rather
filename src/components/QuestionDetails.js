81586e8a2b7635cfdc1e6836e238c1e49e3cca26
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 15:18:08 -0500

    Adds QuestionDetails comp to project

---
+++ src/components/QuestionDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class QuestionDetails extends Component {
+  render() {
+    return (
+      <div>
+        Question Details Page
+      </div>
+    )
+  }
+}
+
+export default QuestionDetails 
