ce2c2460ffad055806b040279d917eb6dec5120b
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 13:46:57 -0500

    Adds basic Login Component Implementation

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Form } from 'react-bootstrap'
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome!</h2>
+        <h3>Sign In</h3>
+        <Form>
+          <Form.Group>
+            <Form.Label>Who are you?</Form.Label>
+            <Form.Control as="select">
+              {this.props.userIds.map((id) => (
+                <option key={id} value={id}>{id}</option>
+              ))}
+              <option>User 1</option>
+              <option>User 2</option>
+              <option>User 3</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ users }) {
+  return {
+    userIds: Object.keys(users)
+  }
+}
+
+export default connect(mapStateToProps)(Login)
