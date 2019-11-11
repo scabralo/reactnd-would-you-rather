9ae44563978dcb069a5e9a3b23f9d413f8c119c4
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 13:48:17 -0500

    Updates App Comp to Implement Login Comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Would You Rather...?</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Would You Rather...?</h1>
+          {console.log(this.props.loading)}
+          {this.props.loading === true 
+            ? "Loading"
+            : <Login /> }
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps({ users }) {
+  return {
+    loading: users === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
