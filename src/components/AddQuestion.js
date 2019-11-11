d85b8c1c21794d6e0e0934147e22196fa59eae55
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-11 15:18:40 -0500

    Adds AddQuestion comp to project

---
+++ src/components/AddQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddQuestion extends Component {
+  render() {
+    return (
+      <div>
+        Add a Question Page
+      </div>
+    )
+  }
+}
+
+export default AddQuestion
