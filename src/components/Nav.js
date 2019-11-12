fdec4b6e5373696677a26e755aab26f7de69dae9
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-12 16:39:08 -0500

    Updates Nav Comp to handle Logout Functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import UserInfo from './UserInfo'
+import { Button } from 'react-bootstrap'
+import { logoutAuthedUser } from '../actions/authedUser'
 
 class Nav extends Component {
+
+  logoutHandler = (e) => {
+    this.props.dispatch(logoutAuthedUser())
+  }
   render() {
-    const { authedUser } = this.props
+    const { authedUser, user } = this.props
 
     return (
       <div>
-        <ul>
-          <li><Link to={authedUser ? '/' : '#' }>Home</Link></li>
-          <li><Link to={authedUser ? '/add' : '#'}>Add Question</Link></li>
-        </ul>
+        <div className='navigation-wrapper'>
+          <ul>
+            <li><Link to={authedUser ? '/' : '#' }>Home</Link></li>
+            <li><Link to={authedUser ? '/add' : '#'}>Add Question</Link></li>
+          </ul>
+        </div>
+        {authedUser && (
+          <div className='user-info-wrapper'>
+          <UserInfo avatarURL={user.avatarURL} authorName={authedUser} />
+          <Button variant="danger" onClick={this.logoutHandler}>Logout</Button>
+        </div>
+        )}
+        
+
       </div>
     )
   }
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps({ authedUser, users }) {
   return {
-    authedUser
+    authedUser,
+    user: users[authedUser] ? users[authedUser] : []
   }
 }
 
