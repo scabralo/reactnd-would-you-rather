37c10eaf43907b43f2b144e52ffcc415e5e61a2a
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-13 16:28:10 -0500

    Implements ranemed NavMenu Comp on Header Comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import Nav from './Nav'
+import NavMenu from './NavMenu'
 
 class Header extends Component {
   render() {
     return (
       <header className="App-header">
         <h1>Would You Rather...?</h1>
-        <Nav />
+        <NavMenu />
       </header>
     )
   }
