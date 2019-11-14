39c1e8753a3663939bdde45647ea6a5b46fdc137
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-14 16:10:56 -0500

    Creates and implements api.js

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer
+} from './_DATA'
+
+export function getInitialData () {
+  return Promise.all([
+    _getUsers(),
+    _getQuestions(),
+  ]).then(([users, questions]) => ({
+    users,
+    questions,
+  }))
+}
+export function saveQuestionAnswer (info) {
+  return _saveQuestionAnswer (info)
+}
+export function saveQuestion (info) {
+  return _saveQuestion (info)
+}
