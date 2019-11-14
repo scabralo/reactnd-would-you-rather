4024c78730cace958b693a64ffc98c287403bdca
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-14 16:10:35 -0500

    Eliminates getInitialDate from _Data file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -119,16 +119,6 @@ function generateUID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
-export function getInitialData () {
-  return Promise.all([
-    _getUsers(),
-    _getQuestions(),
-  ]).then(([users, questions]) => ({
-    users,
-    questions,
-  }))
-}
-
 export function _getUsers () {
   return new Promise((res, rej) => {
     setTimeout(() => res({...users}), 1000)
