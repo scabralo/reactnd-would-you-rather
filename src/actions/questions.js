283aac95088d62998b2b1264d6ece175dcf3ea92
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-15 11:40:29 -0500

    Adds Handlers for RESPOND_QUESTION Action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,29 @@
+import { saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const RESPOND_QUESTION = 'RESPOND_QUESTION'
 
 export function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
+  }
+}
+function respondQuestion (question) {
+  return {
+    type: RESPOND_QUESTION,
+    question
+  }
+}
+export function handleRespondQuestion (qId, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    return saveQuestionAnswer({
+      authedUser, qId, answer
+    })
+      .then((question) => {
+        dispatch(respondQuestion(question))
+      })
   }
 }
