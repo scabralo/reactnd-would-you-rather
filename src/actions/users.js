b04d951d95f8fce500788ff54f8cc92e62ca2cc4
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-15 15:50:13 -0500

    Adds userResponse action handler

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,15 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const USER_RESPONSE = 'USER_RESPONSE'
 
 export function receiveUsers (users) {
   return {
     type: RECEIVE_USERS,
     users
+  }
+}
+export function userResponse (user) {
+  return {
+    type: USER_RESPONSE,
+    user
   }
 }
