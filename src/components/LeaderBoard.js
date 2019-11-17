26a9e57fce1d7a2e7dccf15940339e1d3199a7a9
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-16 21:17:39 -0500

    Implements LeaderBoard Comp

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { ListGroup } from 'react-bootstrap'
+import UserInfo from './UserInfo'
 
 class LeaderBoard extends Component {
   render() {
-    const { authedUser } = this.props
+    const { authedUser, userIds, users } = this.props
     
     if(!authedUser) {
       return <Redirect to='/login' />
     }
 
     return (
-      <div>
-        Leader Board... Oh fuck...
+      <div className='leader-board-wrapper'>
+        <ListGroup >
+          <h2>Leader Board</h2>
+          {userIds.map((id) => (
+            <ListGroup.Item  key={id}>
+              <UserInfo 
+                avatarURL={users[id] ? users[id].avatarURL : ''}
+                authorName={id} 
+              />
+              <div className='user-scores'>
+                <p className='h4'>{`Answers: ${Object.keys(users[id].answers).length}`}</p>
+                <p className='h4'>{`Questions: ${users[id].questions.length}`}</p>
+                <p className='h1'>{`Score: ${Object.keys(users[id].answers).length + users[id].questions.length}`}</p>
+              </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </div>
     )
   }
 }
 
-function mapStateToProps({users, questions, authedUser}) {
+function mapStateToProps({users, authedUser}) {
   return {
     users,
-    questions,
+    userIds : Object.keys(users)
+    .sort((a,b) => {
+      const answersA = Object.keys(users[a].answers).length
+      const questionsA = users[a].questions.length
+
+      const answersB = Object.keys(users[b].answers).length
+      const questionsB = users[b].questions.length
+
+      return (answersB + questionsB) - (answersA + questionsA)
+    }),
     authedUser
   }
 }
