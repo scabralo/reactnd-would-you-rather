f9c11f979d5b08f6ef4c0bc66bf96c1d8c32788e
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-16 23:46:51 -0500

    Turns ErrorPage into a container Component

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default function ErrorPage(props) {
-  const { authedUser } = props
+class ErrorPage extends Component {
+  render() {
+    const { authedUser } = this.props
 
-  if(!authedUser) {
-    return <Redirect to='/login' />
+    if(!authedUser) {
+      return <Redirect to='/login' />
+    }
+
+    return (
+      <div className='error-page'>
+        <h2 className='h1'>404</h2>
+        <h3>Looks like the page you're looking for doesn't exist.</h3>
+      </div>
+    )
   }
-
-  console.log('props: ', props)
-  return (
-    <div className='error-page'>
-      <h2>404</h2>
-      <h3>Looks like the page you're looking for doesn't exist.</h3>
-    </div>
-  )
 }
+
+function mapStateToProps({authedUser}) {
+  return {
+    authedUser
+  }
+}
+
+export default connect(mapStateToProps)(ErrorPage)
