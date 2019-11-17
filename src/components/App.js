2ce3424fc05542b3f4940642d2d1372bcfe9f10d
Sergio Cabral <sergioantonio60@gmail.com>
2019-11-16 22:08:30 -0500

    Adds Switch and ErrorPage to routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 import Header from './Header'
 import Home from './Home'
 import AddQuestion from './AddQuestion'
 import QuestionDetails from './QuestionDetails'
 import LeaderBoard from './LeaderBoard'
+import ErrorPage from './ErrorPage'
 
 class App extends Component {
 
@@ -23,11 +25,14 @@ class App extends Component {
           {this.props.loading === true 
             ? null
             : <div className='content'>
-                <Route path='/' exact component={Home} />
-                <Route path='/login' component={Login} />
-                <Route path='/add' component={AddQuestion} />
-                <Route path='/leaders' component={LeaderBoard} />
-                <Route path='/questions/:id' component={QuestionDetails} />
+                <Switch>
+                  <Route path='/' exact component={Home} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/add' component={AddQuestion} />
+                  <Route path='/leaders' component={LeaderBoard} />
+                  <Route path='/questions/:id' component={QuestionDetails} />
+                  <Route component={ErrorPage} />
+                </Switch>
               </div> }
         </div>
       </Router>
